db3534a3de526a4c70f9c4f2250fe05e937eca16
Amayas-Tamarat <amayas.tamarat@gmail.com>
2023-10-04 17:01:07 +0200

    oue

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -24,38 +24,3 @@ $('.navTrigger').click(function () {
 
 
 
-function elementScroll() {
-    const windowHeight = window.innerHeight;
-
-    // Get the positions of the elements
-    const profilPosition = profil.getBoundingClientRect().top;
-    const portTextPosition = portfolio_text.getBoundingClientRect().top;
-
-    // Check if the elements are within the viewport
-    if (profilPosition < windowHeight) {
-        profil.classList.add('in-view');
-    } else {
-        profil.classList.remove('in-view');
-    }
-
-    if (portTextPosition < windowHeight) {
-        portfolio_text.classList.add('in-view');
-    } else {
-        portfolio_text.classList.remove('in-view');
-    }
-
-    // Loop through the card elements and check their positions
-    const cards = document.querySelectorAll('.cards__item');
-    cards.forEach(card => {
-        const cardPosition = card.getBoundingClientRect().top;
-        if (cardPosition < windowHeight) {
-            card.classList.add('in-view');
-        } else {
-            card.classList.remove('in-view');
-        }
-    });
-}
-
-// Call the elementScroll function on page load and scroll
-window.addEventListener('load', elementScroll);
-window.addEventListener('scroll', elementScroll);
